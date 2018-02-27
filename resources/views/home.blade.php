b03d91cb400325767105a2fa449214fba852bc66
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2018-02-27 14:44:21 -0300

    Fixing maps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,33 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body" style="min-height: 700px">
-                    {!! Mapper::render() !!}
+                <div id="map" class="panel-body" style="min-height: 700px">
+
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+    var map;
+    var center = {lat:  -23.533773, lng:  -46.625290};
+    function initMap() {
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: center,
+            zoom: 10
+        });
+        mainMarker = new google.maps.Marker({
+            position: center,
+            map:map
+        });
+    }
+    function testeMarkers()
+    {
+
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnNmBHNzosBBw32HGR34Qd6JZ4CvmJeWQ&callback=initMap"
+        async defer></script>
+
 @endsection
