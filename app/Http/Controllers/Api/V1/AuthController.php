be010dd9bb515c0399e7599562592f7751ed81b4
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2018-03-23 01:51:44 -0300

    Testing Api

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,38 +4,36 @@ namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Clients;
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $data = $request->toArray();
-        $objInput = \json_decode($data);
-        var_dump($objInput);
-        $cpf = $objInput->cpf;
-        $code = $objInput->code;
-        $deviceImei = $objInput->imei;
-        $deviceBrand = $objInput->brand;
-        $deviceModel = $objInput->model;
+
+        $cpf = $data['cpf'];
+        $code = $data['code'];
+        $deviceImei = $data['imei'];
+        $deviceBrand = $data['brand'];
+        $deviceModel = $data['model'];
         if(empty($cpf) || empty($code) || empty($deviceImei) ) {
              \header('Dados necessários ausentes',true,404);
              die('Xurupita');
 
         }
         else{
-            $client = Client::select()->where('code', $code)->first();
-            var_dump($client->toArray());
-            die();
+            $client = Clients::select()->where('code', 'like', "$code")->first();
+
             if($cpf == $client->cpf) {
                 $token = sha1($code.$cpf.$deviceImei);
                 $client->addDevice($deviceImei, $deviceBrand, $deviceModel, $token);
                 $tokenObj = new \stdClass();
-                $tokenObj->token = $tokenObj;
-                return response(200)->json($tokenObj);
+                $tokenObj->token = $token;
+                return response()->json($tokenObj);
             }
 
         }
-        return reponse(404)->json(false);
+        return response()->json(false);
 
 
     }
