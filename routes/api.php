09e98dfff28a1e93318280786671253fe57e96ed
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2018-04-03 03:24:36 -0300

    Criando Api pra receber pontos

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::group(['prefix'=>'/V1','middleware'=>'api','namespace'=>'Api\V1'],functio
     Route::get('/',function(){
         return response('Olá Mundo');
     });
-    Route::post('/alarm/point', 'AlarmController@point')->name('api.ping.alarm');
+    Route::post('/add/alarm/point', 'AlarmController@point')->name('api.ping.alarm');
     Route::post('/auth','AuthController@login')->name('api.auth');
     Route::post('/alarm', 'AlarmController@start')->name('api.start.alarm');
     Route::post('/help/interact','HelpController@start')->name('api.help.interact');
