954469cecd4deaa67b42a3a68dfedee7be9243c8
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2018-04-21 11:23:58 -0300

    Fix Ajax call

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -37,34 +37,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 require('jquery');
 var maxAlarm = 0;
 var maxHelp = 0;
-$(document).on('ready', function(){
-    $.ajax({
-        url: '/api/V1/calls',
-        method:'get',
-        dataType:'json',
-    }).done(function(data){
-        console.dir(data);
-        $("#callNumbers").html(data.openCalls);
-        maxAlarm = data.lastAlarm;
-        maxHelp = data.lastHelp;
-    });
 
-
-    window.setTimeout(function(){
-        $.ajax({
-            url: '/api/V1/lastcall/'+maxAlarm+'/'+maxHelp,
-            method:'get',
-            dataType:'json'
-        }).done(function (data) {
-            console.dir(data);
-        $("#callNumbers").html(data.openCalls);
-        if(data.lastAlarm>maxAlarm || data.lastHelp>maxHelp){
-            new Audio('/alarm.mp3').play();
-            maxAlarm = data.lastAlarm;
-            maxHelp = data.lastHelp;
-            alert('Você tem novos chamados');
-            }
-        });
-    }, 1000);
-});
 
