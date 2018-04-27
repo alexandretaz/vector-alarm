0f41acb515df6aa402717252353dd4a98810c5f5
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2018-04-27 05:23:49 -0300

    Changing home page to show all points

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,7 +71,7 @@
             var latLng = new google.maps.LatLng(latitude, longitude)
 
             map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 6,
+                zoom: 3,
                 center:latLng
             });
             putPoints();
